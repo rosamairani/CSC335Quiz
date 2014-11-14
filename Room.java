0a1ef867a599b3fc1aed01169f689dfdde101804
aromero32 <aromero32@email.arizona.edu>
2014-11-14 11:48:42 -0700

    All I did with this was take off the broken line

--- Room.java
+++ Room.java
@@ -1,5 +1,3 @@
-package stuff;
-
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,45 +5,35 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Room 
-{
+public class Room {
 	private BufferedImage image;
 
 	private boolean obstruction;
 	private boolean damage;
 	private boolean hasUnit;
-	
-	public Room(String fileName, boolean obstruction, boolean damage, boolean hasUnit)
-	{
+
+	public Room(String fileName, boolean obstruction, boolean damage,
+			boolean hasUnit) {
 		try {
 			image = ImageIO.read(new File(fileName));
 		} catch (IOException ex) {
 			// handle exception...
 		}
-		
+
 		this.obstruction = obstruction;
 		this.damage = damage;
 		this.hasUnit = hasUnit;
 	}
 
-	public Image getImage() 
-	{
+	public Image getImage() {
 		return image;
 	}
 
 	/*
 	 * returns if a unit is allowed to move in map room
 	 */
-	public boolean getCanMove() 
-	{
+	public boolean getCanMove() {
 		return obstruction || hasUnit;
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
